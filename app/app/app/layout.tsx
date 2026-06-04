d236bf93e40937435616bd0c694d6c520bd0e525
ninja-choba <asianchampions@yahoo.co.jp>
2026-06-04 21:02:27 +0900

    Update layout.tsx

--- app/app/app/layout.tsx
+++ app/app/app/layout.tsx
@@ -34,7 +34,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja" style="background:#FFFFFF;color-scheme:light;">
+    <html lang="ja">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
