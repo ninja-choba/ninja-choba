a23b288165b7d149c607e0cc815f3b0a39a3967d
ninja-choba <asianchampions@yahoo.co.jp>
2026-06-06 07:53:25 +0900

    Delete app directory

--- app/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true },
-  turbopack: {},  // Turbopackを明示的に有効化（webpack競合を回避）
-}
-
-module.exports = nextConfig
